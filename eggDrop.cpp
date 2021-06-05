40d4407e313d9aeb0d736cb1cadaa1d54199d2fa
Saurabh Verma <77957804+saurabhv2@users.noreply.github.com>
2021-06-05 17:27:26 +0530

    Update eggDrop.cpp

--- eggDrop.cpp
+++ eggDrop.cpp
@@ -34,3 +34,42 @@ int main()
 	}
 	return 0;
 }
+
+
+//Memoization
+class Solution
+{
+    public:
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    vector<vector<int>> dp;
+    int help(int n, int k) {
+        if(k<=1) return 1;
+        if(n==1) return k;
+        if(dp[n][k] != -1) return dp[n][k];
+        
+        int ans=INT_MAX;
+        for(int x=k-1;x>0;x--) {
+            ans = min(ans, max(help(n, k-x), help(n-1, x-1)) + 1);
+        }
+        return dp[n][k] = ans;
+    }
+    int eggDrop(int n, int k) 
+    {
+        // your code here || n=egg, k=floor
+        dp.resize(n+1, vector<int>(k+1, -1));
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<k;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++) {
+            dp[i][1]=1;
+        }
+        for(int i=1;i<=k;i++) {
+            dp[1][i]=i;
+        }
+        return help(n, k);
+    }
+};
